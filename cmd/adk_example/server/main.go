015a34965d49552b8fff30213d542ecf655622b2
ThisaraWeerakoon <thisara.weerakoon2001@gmail.com>
2025-12-14 19:17:22 +0530

    add remote client main

--- cmd/adk_example/server/main.go
+++ cmd/adk_example/server/main.go
@@ -2,46 +2,30 @@ package main
 
 import (
 	"context"
-	"iter"
 	"log"
+	"os"
 
 	mesh_adk "github.com/ThisaraWeerakoon/Agent-Mesh/pkg/adk"
 	"google.golang.org/adk/agent/llmagent"
-	"google.golang.org/adk/model"
+	"google.golang.org/adk/model/gemini"
 	"google.golang.org/genai"
 )
 
-// MockModel implements a basic model for testing purposes since we don't have a real LLM connected.
-type MockModel struct {
-}
 
-func (m *MockModel) Name() string {
-	return "mock-server-model"
-}
-
-func (m *MockModel) GenerateContent(ctx context.Context, req *model.LLMRequest, stream bool) iter.Seq2[*model.LLMResponse, error] {
-	return func(yield func(*model.LLMResponse, error) bool) {
-		resp := &model.LLMResponse{
-			Content: &genai.Content{
-				Parts: []*genai.Part{
-					{
-						Text: "This is a summarized text from the remote agent.",
-					},
-				},
-			},
-		}
-		yield(resp, nil)
-	}
-}
 
 func main() {
-	// 1. Create your ADK Agent as usual
-	// Using a mock model for the example to be runnable without API keys
-	mockModel := &MockModel{}
+
+	ctx := context.Background()
+	geminiModel, err := gemini.NewModel(ctx, "gemini-2.5-flash-lite", &genai.ClientConfig{
+		APIKey: os.Getenv("GOOGLE_API_KEY"),
+	})
+	if err != nil {
+		log.Fatalf("Failed to create model: %v", err)
+	}
 
 	summaryAgent, err := llmagent.New(llmagent.Config{
 		Name:        "summary_agent",
-		Model:       mockModel,
+		Model:       geminiModel,
 		Description: "Agent to summarize text.",
 		Instruction: `You are an expert summarizer.`,
 	})
